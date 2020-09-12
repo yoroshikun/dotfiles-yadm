a2988a38287637147fd620c930b0cf7f3ff9099e
Drew Hutton <drew.business2308@gmail.com>
2020-09-12 10:57:00 +0930

    fix: tweak kitty font size and fix fish install on linux

--- .config/yadm/scripts/fish.sh
+++ .config/yadm/scripts/fish.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+system_type=$(uname -s)
+
 # install fish
-echo /usr/local/bin/fish | tee -a /etc/shells
-# set fish as the default
-chsh -s /usr/local/bin/fish
+if [ "$system_type" = "Darwin" ]; then
+    echo /usr/local/bin/fish | sudo tee -a /etc/shells
+    chsh -s /usr/local/bin/fish
+fi
+
+if [ "$system_type" = "Linux" ]; then
+    echo /home/linuxbrew/.linuxbrew/bin/fish | sudo tee -a /etc/shells
+    chsh -s /home/linuxbrew/.linuxbrew/bin/fish
+fi
+

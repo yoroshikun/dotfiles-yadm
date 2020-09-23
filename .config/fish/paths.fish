992d90755670a02aa14bfb1784a0d1939a6234f2
Drew Hutton <drew.business2308@gmail.com>
2020-09-23 10:21:08 +0930

    feat: add platformsh, nitro and rust

--- .config/fish/paths.fish
+++ .config/fish/paths.fish
@@ -3,3 +3,6 @@ set -g fish_user_paths $fish_user_paths '/usr/local/sbin'
 
 # yarn
 set -g fish_user_paths $fish_user_paths "(yarn global bin)/node_modules/.bin"
+
+# platformsh
+set -U fish_user_paths $fish_user_paths $HOME/.platformsh/bin
--- .config/yadm/scripts/bash.sh
+++ .config/yadm/scripts/bash.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo bash <(curl -sLS http://installer.getnitro.sh)
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+curl -sS https://platform.sh/cli/installer | php
